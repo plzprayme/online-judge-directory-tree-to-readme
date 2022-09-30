30629856e5b32e3f073b76fd2ef0a5f783944454
Kayoung <rkdkdudjd@gmail.com>
2022-09-30 19:08:02 +0900

    파일명 띄어쓰기 지원

--- src/main/java/makrdown/MDHelper.java
+++ src/main/java/makrdown/MDHelper.java
@@ -1,13 +1,28 @@
 package makrdown;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 public class MDHelper {
 
     public static String H1(String s) {
         return "# " + s;
     }
 
+    public static String encode(String url) {
+        String[] splitScheme = url.split("://");
+
+        StringBuilder encodedUrl = new StringBuilder();
+        encodedUrl.append(splitScheme[0]).append(":/");
+
+        for (String plaintext : splitScheme[1].split("/")) {
+            encodedUrl.append("/").append(URLEncoder.encode(plaintext, StandardCharsets.UTF_8).replace("+", "%20"));
+        }
+        return encodedUrl.toString();
+    }
+
     public static String link(String s, String url) {
-        return "[" + s + "]" + "(" + url + ")";
+        return "[" + s + "]" + "(" + encode(url) + ")";
     }
 
     public static String list(String s) {
